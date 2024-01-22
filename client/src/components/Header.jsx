d6097cabe036c560cd3f47b8aa27aaee6f566034
HarshadSanjayKarale <harshadkarale0@gmail.com>
2024-01-22 11:25:32 +0530

    Connection to the db

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -32,7 +32,7 @@ export default function Header() {
             <FaMoon/>
           </Button>
           <Link to='/sign-in'>
-          <Button gradientDuoTone='purpleToBlue'>
+          <Button gradientDuoTone='purpleToBlue' outline>
             SIGN IN
           </Button>
           </Link>
